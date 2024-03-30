e09e42e7421f398a1a84b7b958480c24cb0ca770
WhiteArct1c <matrodrigues1576@gmail.com>
2024-03-30 18:15:53 -0300

    feat: creating the credit card flags repository

---
+++ src/main/java/com/laresencanto/laresencantorestapi/repository/CreditCardFlagsRepository.java
@@ -0,0 +1,9 @@
+package com.laresencanto.laresencantorestapi.repository;
+
+import com.laresencanto.laresencantorestapi.domain.CreditCardFlags;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CreditCardFlagsRepository extends JpaRepository<CreditCardFlags, Long> {
+}
