f3c38419a152e86521ce2c5597fdae565f78f0f1
WhiteArct1c <matrodrigues1576@gmail.com>
2024-03-30 18:13:05 -0300

    fix: add the repository spring annotation

--- src/main/java/com/laresencanto/laresencantorestapi/repository/CreditCardRepository.java
+++ src/main/java/com/laresencanto/laresencantorestapi/repository/CreditCardRepository.java
@@ -2,10 +2,12 @@ package com.laresencanto.laresencantorestapi.repository;
 
 import com.laresencanto.laresencantorestapi.domain.CreditCard;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface CreditCardRepository extends JpaRepository<CreditCard, Long> {
     Optional<List<CreditCard>> findAllByCustomerId (Long id);
 }
