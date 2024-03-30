19646ba93b8ce4ecbe7740794e6ff90fff8a5af2
WhiteArct1c <matrodrigues1576@gmail.com>
2024-03-30 18:15:36 -0300

    refactor: adjusting the enum variable to final

--- src/main/java/com/laresencanto/laresencantorestapi/utils/enums/UserRole.java
+++ src/main/java/com/laresencanto/laresencantorestapi/utils/enums/UserRole.java
@@ -1,16 +1,16 @@
 package com.laresencanto.laresencantorestapi.utils.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum UserRole {
     ADMIN("admin"),
     USER("user");
 
-    private String role;
+    private final String role;
 
     UserRole(String role){
         this.role = role;
     }
 
-    public String getRole(){
-        return role;
-    }
 }
