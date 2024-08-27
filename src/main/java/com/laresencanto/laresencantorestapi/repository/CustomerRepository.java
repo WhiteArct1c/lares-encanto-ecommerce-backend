f0b9c97edcbe061311949b2db8d3cac6a662fee9
WhiteArct1c <matrodrigues1576@gmail.com>
2024-08-26 22:39:05 -0300

    refact: adjusting the imports

--- src/main/java/com/laresencanto/laresencantorestapi/repository/CustomerRepository.java
+++ src/main/java/com/laresencanto/laresencantorestapi/repository/CustomerRepository.java
@@ -1,11 +1,10 @@
 package com.laresencanto.laresencantorestapi.repository;
 
-import java.util.Optional;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.laresencanto.laresencantorestapi.domain.Customer;
 import com.laresencanto.laresencantorestapi.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
     Optional<Customer> findByFullName(String fullName);
