fd0d05fc1c7fb398cd9ff9bc56e4a40c799cdb53
WhiteArct1c <matrodrigues1576@gmail.com>
2025-02-19 00:45:23 -0300

    feat: add sql script insert some product categories on db

---
+++ src/main/resources/db/migration/V19__insert-product-categories-values.sql
@@ -0,0 +1,11 @@
+INSERT INTO product_categories (name, created_at, updated_at) VALUES
+    ('Cozinha', NOW(), NOW()),
+    ('Banheiro', NOW(), NOW()),
+    ('Quarto', NOW(), NOW()),
+    ('Sala de Estar', NOW(), NOW()),
+    ('Sala de Jantar', NOW(), NOW()),
+    ('Escritório', NOW(), NOW()),
+    ('Área Externa', NOW(), NOW()),
+    ('Lavanderia', NOW(), NOW()),
+    ('Garagem', NOW(), NOW()),
+    ('Infantil', NOW(), NOW());
