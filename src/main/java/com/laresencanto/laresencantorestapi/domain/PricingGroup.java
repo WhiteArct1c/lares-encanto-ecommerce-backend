9cbbf15d0333f953f21d82c4a7343cbc1aed6745
WhiteArct1c <matrodrigues1576@gmail.com>
2025-02-19 22:09:03 -0300

    fix: adjusting table name

--- src/main/java/com/laresencanto/laresencantorestapi/domain/PricingGroup.java
+++ src/main/java/com/laresencanto/laresencantorestapi/domain/PricingGroup.java
@@ -7,7 +7,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "pricing_group")
+@Table(name = "pricing_groups")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
--- src/main/resources/db/migration/V22__create-pricing-group-table.sql
+++ src/main/resources/db/migration/V22__create-pricing-group-table.sql
@@ -1,4 +1,4 @@
-CREATE TABLE pricing_group (
+CREATE TABLE pricing_groups (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     profit_margin DECIMAL(5,2) NOT NULL CHECK (profit_margin >= 0)
